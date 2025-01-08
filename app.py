53f1f725297428a6ce7ad8f12e7f38efa440fa5b
lavender0106-hi <lavenderhaze0106@gmail.com>
2025-01-08 20:58:11 +0000

    added new pages,  dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-
+JOBS =[
+    {
+        'id':1,
+        'totalIncome': '100',
+        'totalExpenses': '50',
+        'totalBalance': '50',
+        'expensesBar':'50',
+        'category': 'Food',
+        'amount':'10',
+        'notes': 'groceries'
+        
+    }
+]
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
-
+    return render_template('dashboard.html',
+                          jobs=JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS) 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', debug=True)
